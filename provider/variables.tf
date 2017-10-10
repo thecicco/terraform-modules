df5920c0426ca903992675472f80ebc31f98266c
jsecchiero <secchierojacopo@gmail.com>
2017-10-10 14:53:03 +0200

    fix username variables

--- provider/variables.tf
+++ provider/variables.tf
@@ -2,8 +2,8 @@ variable "region" {
   default = "it-mil1"
 }
 variable "auth_url" {
-  default = "https://api.${var.region}.entercloudsuite.com/v2.0"
+  default = "https://api.it-mil1.entercloudsuite.com/v2.0"
 }
-variable tenant_name {}
-variable user_name {}
-variable password {}
+variable "tenant_name" {}
+variable "user_name" {}
+variable "password" {}
