a0c90ac50f1454160d914275a8f6cdb54681b9d5
jsecchiero <secchierojacopo@gmail.com>
2017-11-13 16:03:46 +0100

    remove split funcion on external_vip module

--- external_vip/main.tf
+++ external_vip/main.tf
@@ -11,7 +11,7 @@ resource "openstack_networking_port_v2" "port_public" {
 }
 
 resource "openstack_networking_floatingip_v2" "port_public_floating_ip" {
-  count = "${length(split(",", var.external_vips))}"
+  count = "${length(var.external_vips)}"
   pool = "PublicNetwork"
   port_id = "${openstack_networking_port_v2.port_public.id}"
   region = "${var.region}"
