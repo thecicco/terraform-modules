b3a2bec41e1c995f0e15fe85c48efb65730eb4e3
jsecchiero <secchierojacopo@gmail.com>
2017-11-13 15:43:36 +0100

    add variable network_id

--- external_vip/main.tf
+++ external_vip/main.tf
@@ -1,7 +1,7 @@
 resource "openstack_networking_port_v2" "port_public" {
   name = "port_public"
   count = "${length(split(",", var.external_vips))}"
-  network_id = "${data.openstack_networking_network_v2.instance_network.id}"
+  network_id = "${var.network_id}"
   admin_state_up = "true"
   region = "${var.region}"
   fixed_ip = {
