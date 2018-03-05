df491f5c9a21f5dd2444d4380fd4975d5302a771
jsecchiero <secchierojacopo@gmail.com>
2018-03-05 17:39:29 +0100

    resolve interpolation issue

--- postgres/variables.tf
+++ postgres/variables.tf
@@ -11,7 +11,7 @@ variable "network_name" {}
 variable "image" {}
 
 variable "image_slave" {
-    default = "${var.image}"
+    default = "GNU/Linux Ubuntu Server 16.04 Xenial Xerus x64"
 }
 
 variable "slave_count" {
