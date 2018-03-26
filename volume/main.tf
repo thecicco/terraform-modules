7470214366ebd7569fa607686ce63c14151e3038
Davide Panarese <davide.panarese@outlook.com>
2018-03-26 10:51:13 +0200

    adding device attachments variable

--- volume/main.tf
+++ volume/main.tf
@@ -10,6 +10,7 @@ resource "openstack_compute_volume_attach_v2" "va" {
   count = "${var.quantity}"
   instance_id = "${var.instance[count.index]}"
   volume_id = "${openstack_blockstorage_volume_v2.volume.*.id[count.index]}"
+  device = "${var.device}"
   region = "${var.region}"
 
   # Workaround avoid attachment recreation on each quantity change
