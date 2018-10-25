d88ec7a79f3e0c22140a638087a2eb303675d712
Attilio Greco <attilio.greco.gapmilano@gmail.com>
2018-10-25 15:56:50 +0200

    update playbook

--- openstack/prometheus/variables.tf
+++ openstack/prometheus/variables.tf
@@ -79,7 +79,7 @@ scrape_configs:
       regex: '(metrics.*)'
       action: keep
     - source_labels: ['__meta_consul_service']
-      regex: '(metrics.*)'
+      regex: 'metrics(.*)'
       target_label:  'job'
       replacement:   '$1'
     - source_labels: ['__meta_consul_node']
@@ -90,8 +90,9 @@ scrape_configs:
       regex:         ',(production|canary),'
       target_label:  'group'
       replacement:   '$1'
-}
 EOF
+}
+
 variable "prometheus_alertmanager_conf_main" {
   default = <<EOF
 global:
@@ -150,100 +151,8 @@ groups:
     labels:
       severity: critical
     annotations:
-      summary: "Instance {{ $labels.instance }} down"
-      description: "{{ $labels.instance }} of job {{ $labels.job }} has been down for more than 30 seconds."
- 
- - record: instance:node_cpu:rate:sum  
-    expr: sum(rate(node_cpu{mode!="idle",mode!="iowait",mode!~"^(?:guest.*)$"}[3m]))
-      BY (instance)
-  - record: instance:node_filesystem_usage:sum
-    expr: sum((node_filesystem_size{mountpoint="/"} - node_filesystem_free{mountpoint="/"}))
-      BY (instance)
-  - record: instance:node_network_receive_bytes:rate:sum
-    expr: sum(rate(node_network_receive_bytes[3m])) BY (instance)
-  - record: instance:node_network_transmit_bytes:rate:sum
-    expr: sum(rate(node_network_transmit_bytes[3m])) BY (instance)
-  - record: instance:node_cpu:ratio
-    expr: sum(rate(node_cpu{mode!="idle"}[5m])) WITHOUT (cpu, mode) / ON(instance)
-      GROUP_LEFT() count(sum(node_cpu) BY (instance, cpu)) BY (instance)
-  - record: cluster:node_cpu:sum_rate5m
-    expr: sum(rate(node_cpu{mode!="idle"}[5m]))
-  - record: cluster:node_cpu:ratio
-    expr: cluster:node_cpu:rate5m / count(sum(node_cpu) BY (instance, cpu))
- 
- - alert: NodeDiskRunningFullIn24Hours  
-    expr: predict_linear(node_filesystem_free{device=~"/dev/.*",mountpoint!~"/var/lib/docker.*"}[6h], 24 * 3600) < 0
-    for: 30m
-    labels:
-      severity: warning
-    annotations: 
-      summary: "Disk will be Full in 24 hours"
-      description: Mount point {{$labels.mountpoint}} on device {{$labels.device}} on node {{$labels.instance}} is running full within the next 24 hours.
- 
- - alert: NodeDiskRunningFullIn2Hours  
-    expr: predict_linear(node_filesystem_free{device=~"/dev/.*",mountpoint!~"/var/lib/docker.*"}[3h], 2 * 3600) < 0
-    for: 10m
-    labels:
-      severity: critical
-    annotations: 
-      summary: "Disk will be Full in 2 hours"
-      description: Mount point {{$labels.mountpoint}} on device {{$labels.device}} on node {{$labels.instance}} is running full within the next 2 hours.
- 
- - alert: NodeDiskRunningFull  
-    expr: node_filesystem_avail{mountpoint!~"/var/lib/docker.*"} / node_filesystem_size{mountpoint!~"/var/lib/docker.*"} * 100 < 3 
-    for: 10m
-    labels:
-      severity: critical
-      annotations: 
-      summary: "Disk Full at {{ $value }} %"
-      description: device {{$labels.device}} on node {{$labels.instance}} is full at {{ $value }} %
- 
- - alert: ram_full_more_than_80  
-    expr: 100 - ((node_memory_MemAvailable * 100) / node_memory_MemTotal) > 80
-    for: 2m
-    labels:
-      severity: warning
-    annotations:
-      summary: "ram full {{ humanize $value}}%"
-      description: "{{$labels.instance}} ram is full at {{$value | humanize }}%."
-groups:
-- name: memcache
-  rules:
-  - alert: Probe-down
-    expr: probe_success == 0
-    for: 30s
-    labels:
-      severity: warning
-    annotations:
-      summary: "Instance probe {{ $labels.instance }} is down"
-      description: "{{ $labels.instance }} of job {{ $labels.job }} has been down for more than 30 seconds."
- 
- - alert: backendDown75%  
-    expr: sum(probe_success{job="blackbox-http_check"}) / count(probe_success{job="blackbox-http_check"}) * 100 < 75
-    for: 30s
-    labels:
-      severity: warning
-    annotations:
-      summary: "{{ $value }} of backend are down"
-      description: "{{ $value }} of backend are down"
- 
- - alert: backendDown50%  
-    expr: sum(probe_success{job="blackbox-http_check"}) / count(probe_success{job="blackbox-http_check"}) * 100 < 75
-    for: 30s
-    labels:
-      severity: warning
-    annotations:
-      summary: "{{ $value }} of backend are down"
-      description: "{{ $value }} of backend are down"
- 
- - alert: ssl_certificate  
-    expr: probe_ssl_earliest_cert_expiry-time() < 604800
-    for: 30s
-    labels:
-      severity: weekly
-    annotations:
-      summary: "ssl certificate expiring in {{ $value | humanizeDuration }}"
-      description: "ssl certificate expiring in {{ $value | humanizeDuration }}"
+      summary: 'Instance down'
+      description: 'Instance has been down for more than 30 seconds.'
 EOF
 }
 
