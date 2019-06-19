57aa23640cb665cf0429f9225ece9cedfb1d7cfa
jsecchiero <secchierojacopo@gmail.com>
2019-06-19 11:15:01 +0200

    Avoid conflict during parallel upload of the same openstack image

--- openstack/instance/image_sync.sh
+++ openstack/instance/image_sync.sh
@@ -11,7 +11,7 @@ else
   OUTPUT=$(
   until curl -s https://swift.entercloudsuite.com/v1/KEY_1a68c22a99cd4e558054ede2c878929d/automium-catalog-images/openstack/${IMAGE}.qcow2 | openstack image create ${IMAGE} -f json; do
     sleep 10
-    openstack image list -f json | jq -r "map(select(.Name==\"${IMAGE}\")) | .[].ID" | while read uuid; do openstack image delete $uuid; done
+    openstack image list -f json | jq -r "map(select(.Name==\"${IMAGE}\")) | map(select(.Status!=\"saving\")) | map(select(.Status!=\"active\")) | .[0] | .ID | select(.!=null)" | while read uuid; do openstack image delete $uuid; done
   done
   )
   IMAGE_UUID=$(openstack image list -f json | jq -r "map(select(.Name==\"${IMAGE}\")) | map(select(.Status==\"active\")) | .[0] | .ID | select(.!=null)")
