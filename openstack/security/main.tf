9cc2dd8c1ae171f584752039181fb13fdf3b0711
jsecchiero <secchierojacopo@gmail.com>
2019-08-12 17:35:36 +0200

    Add ignore changes on openstack secuirty group and fix indent

--- openstack/security/main.tf
+++ openstack/security/main.tf
@@ -1,12 +1,17 @@
 resource "openstack_networking_secgroup_v2" "secgroup_public" {
-    name = "${var.name}"
-    description ="Security group ${var.name}"
-    delete_default_rules = true
-    region = "${var.region}"
+  name = "${var.name}"
+  description ="Security group ${var.name}"
+  delete_default_rules = true
+  region = "${var.region}"
+
+  lifecycle {
+    ignore_changes = ["*"]
+    prevent_destroy = true
+  }
 }
 
 output "sg_id" {
-    value = "${openstack_networking_secgroup_v2.secgroup_public.id}"
+  value = "${openstack_networking_secgroup_v2.secgroup_public.id}"
 }
 
 output "sg_name" {
@@ -14,21 +19,31 @@ output "sg_name" {
 }
 
 resource "openstack_networking_secgroup_rule_v2" "in" {
-    region = "${var.region}"
-    direction = "ingress"
-    ethertype = "IPv4"
-    protocol = "${var.protocol}"
-    port_range_min = "${var.port_range_min}"
-    port_range_max = "${var.port_range_max}"
-    remote_ip_prefix = "${var.allow_remote}"
-    security_group_id = "${openstack_networking_secgroup_v2.secgroup_public.id}"
+  region = "${var.region}"
+  direction = "ingress"
+  ethertype = "IPv4"
+  protocol = "${var.protocol}"
+  port_range_min = "${var.port_range_min}"
+  port_range_max = "${var.port_range_max}"
+  remote_ip_prefix = "${var.allow_remote}"
+  security_group_id = "${openstack_networking_secgroup_v2.secgroup_public.id}"
+
+  lifecycle {
+    ignore_changes = ["*"]
+    prevent_destroy = true
+  }
 }
 
 resource "openstack_networking_secgroup_rule_v2" "out" {
-    region = "${var.region}"
-    direction = "egress"
-    ethertype = "IPv4"
-    protocol = ""
-    remote_ip_prefix = "0.0.0.0/0"
-    security_group_id = "${openstack_networking_secgroup_v2.secgroup_public.id}"
+  region = "${var.region}"
+  direction = "egress"
+  ethertype = "IPv4"
+  protocol = ""
+  remote_ip_prefix = "0.0.0.0/0"
+  security_group_id = "${openstack_networking_secgroup_v2.secgroup_public.id}"
+
+  lifecycle {
+    ignore_changes = ["*"]
+    prevent_destroy = true
+  }
 }
