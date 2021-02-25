01391cc0ed4046cff94689004c75f0e764eae2a5
Lorenzo Comotti <lcomotti@enter.eu>
2021-02-25 10:37:51 +0100

    (opensack-volume) upgrade module

--- openstack/volume/main.tf
+++ openstack/volume/main.tf
@@ -1,19 +1,19 @@
 resource "openstack_blockstorage_volume_v2" "volume" {
-  count = "${var.quantity}"
-  name = "${var.name}"
-  size = "${var.size}"
-  volume_type = "${var.volume_type}"
-  region = "${var.region}"
+  count = var.quantity
+  name = var.name
+  size = var.size
+  volume_type = var.volume_type
+  region = var.region
 }
 
 resource "openstack_compute_volume_attach_v2" "va" {
-  count = "${var.quantity}"
-  instance_id = "${var.instance[count.index]}"
-  volume_id = "${openstack_blockstorage_volume_v2.volume.*.id[count.index]}"
-  region = "${var.region}"
+  count = var.quantity
+  instance_id = var.instance[count.index]
+  volume_id = openstack_blockstorage_volume_v2.volume.*.id[count.index]
+  region = var.region
 
   # Workaround avoid attachment recreation on each quantity change
   lifecycle {
-    ignore_changes = true
+    ignore_changes = all
   }
 }
