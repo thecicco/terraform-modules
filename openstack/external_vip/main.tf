e6fe1d06db8dc66201b577a803afa7c4873690ca
Lorenzo Comotti <lcomotti@enter.eu>
2021-03-12 11:44:46 +0100

    add terraform provider in vip's modules

--- openstack/external_vip/main.tf
+++ openstack/external_vip/main.tf
@@ -1,3 +1,13 @@
+terraform {
+required_version = ">= 0.14.0"
+  required_providers {
+    openstack = {
+      source  = "terraform-provider-openstack/openstack"
+      version = "~> 1.35.0"
+    }
+  }
+}
+
 data "openstack_networking_network_v2" "network" {
   region = var.region
   name = var.network_name
