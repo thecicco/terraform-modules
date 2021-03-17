ca391d45bb10d2fba53bf24a1981628bf606baf9
jsecchiero <secchierojacopo@gmail.com>
2021-03-17 18:39:21 +0100

    Add image checksum

--- openstack/instance/image_sync.sh
+++ openstack/instance/image_sync.sh
@@ -4,19 +4,41 @@ set -o pipefail
 
 TIMEOUT=2700
 
-# TODO: Replace it with a file lock when we have shared volume between jobs
-# In case of multiple run, try to avoid multiple image upload
-sleep $(($(shuf -i 0-5 -n 1) * 7))
+checksum() {
+  if [ -z ${IMAGE_UUID} ]; then
+    return
+  fi
+  IMAGE_CHECKSUM=$(openstack image show -f json $IMAGE_UUID  | jq -r '.checksum')
+  IMAGE_REMOTE_CHECKSUM=$(curl -s https://swift.entercloudsuite.com/v1/KEY_1a68c22a99cd4e558054ede2c878929d/automium-catalog-images/openstack/${IMAGE}.qcow2.md5sum)
+  # Check that is not an empty string and that is a md5 like string
+  if [ "${IMAGE_REMOTE_CHECKSUM}" ] && \
+     [ "$(echo ${IMAGE_REMOTE_CHECKSUM} | wc -m)" == "33" ]; then
+    if [ "${IMAGE_CHECKSUM}" != "${IMAGE_REMOTE_CHECKSUM}" ]; then
+      >&2 echo "Checksum of the image ${IMAGE} is not consistent"
+      openstack image delete ${IMAGE_UUID}
+      IMAGE_UUID=""
+    else
+      >&2 echo "Checksum of the image ${IMAGE} is valid"
+    fi
+  fi
+}
 
-IMAGE_UUID="$(openstack image list -f json | jq -r "map(select(.Name==\"${IMAGE}\")) | map(select(.Status==\"active\")) | .[0] | .ID | select (.!=null)")"
+
 while [ -z "${IMAGE_UUID}" ]; do
+  IMAGE_UUID="$(openstack image list -f json | jq -r "map(select(.Name==\"${IMAGE}\")) | map(select(.Status==\"active\")) | .[0] | .ID | select (.!=null)")"
   OUTPUT=""
 
   if [ -n "${IMAGE_UUID}" ]; then
     OUTPUT="${IMAGE} already exist, skip image upload"
+    checksum
   else
     IMAGE_UUID=$(
     >&2 echo "Starting image check for ${IMAGE}"
+
+    # TODO: Replace it with a file lock when we have shared volume between jobs
+    # In case of multiple run, try to avoid multiple image upload
+    sleep $(($(shuf -i 0-5 -n 1) * 7))
+
     # If another upload is running wait 5 min
     IMAGE_CREATED_NEWEST_ELAPSED=0
     while [ ${IMAGE_CREATED_NEWEST_ELAPSED} -lt ${TIMEOUT} ]; do
@@ -31,6 +53,7 @@ while [ -z "${IMAGE_UUID}" ]; do
       done
       IMAGE_CREATED_NEWEST_ELAPSED=$(echo $(date "+%s") - ${IMAGE_CREATED_NEWEST} | bc)
       IMAGE_UUID="$(openstack image list -f json | jq -r "map(select(.Name==\"${IMAGE}\")) | map(select(.Status==\"active\")) | .[0] | .ID | select (.!=null)")"
+      checksum
       if [ ${IMAGE_UUID} ]; then
         echo ${IMAGE_UUID}
         exit 0
@@ -51,10 +74,14 @@ while [ -z "${IMAGE_UUID}" ]; do
 
     IMAGE_UUID=$(openstack image list -f json | jq -r "map(select(.Name==\"${IMAGE}\")) | map(select(.Status==\"active\")) | .[0] | .ID | select(.!=null)")
     openstack image set --property hw_vif_multiqueue_enabled='true' ${IMAGE_UUID} > /dev/null
+
+    checksum
+
     echo ${IMAGE_UUID}
     )
   fi
 done
+
 
 OUTPUT=$(echo $OUTPUT | cut -c 1-20)
 jq -r -n --arg image_uuid "${IMAGE_UUID}" --arg output "${OUTPUT}" '{"image_uuid":$image_uuid,"output":$output}'
