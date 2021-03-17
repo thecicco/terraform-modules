5fb8967aacc2cd13233b2a44065817eda2880fbe
jsecchiero <secchierojacopo@gmail.com>
2021-03-17 17:25:36 +0100

    Improving resiliency and parallelism of image upload

--- openstack/instance/image_sync.sh
+++ openstack/instance/image_sync.sh
@@ -2,43 +2,59 @@
 
 set -o pipefail
 
-IMAGE_UUID="$(openstack image list -f json | jq -r "map(select(.Name==\"${IMAGE}\")) | map(select(.Status==\"active\")) | .[0] | .ID | select (.!=null)")"
-OUTPUT=""
+TIMEOUT=2700
 
-if [ -n "${IMAGE_UUID}" ]; then
-  OUTPUT="${IMAGE} already exist, skip image upload"
-else
-  IMAGE_UUID=$(
-  # If another upload is running wait 5 min
-  IMAGE_CREATED_NEWEST_ELAPSED=0
-  while [ ${IMAGE_CREATED_NEWEST_ELAPSED} -lt 300 ]; do
-    [ $IMAGE_CREATED_NEWEST_ELAPSED -ne 0 ] && sleep 10
-    IMAGES_SAVING_UUID="$(openstack image list -f json | jq -r "map(select(.Name==\"${IMAGE}\")) | map(select(.Status==\"saving\")) | .[].ID | select (.!=null)")"
-    IMAGE_CREATED_NEWEST=0
-    for IMAGE_SAVING_UUID in $IMAGES_SAVING_UUID; do
-      IMAGE_CREATED=$(openstack image show -f json ${IMAGE_SAVING_UUID} | jq -r .created_at | xargs -i date "+%s" -d "{}")
-      if [ ${IMAGE_CREATED} -gt ${IMAGE_CREATED_NEWEST} ]; then
-        IMAGE_CREATED_NEWEST=${IMAGE_CREATED}
+# TODO: Replace it with a file lock when we have shared volume between jobs
+# In case of multiple run, try to avoid multiple image upload
+sleep $(($(shuf -i 0-5 -n 1) * 7))
+
+IMAGE_UUID="$(openstack image list -f json | jq -r "map(select(.Name==\"${IMAGE}\")) | map(select(.Status==\"active\")) | .[0] | .ID | select (.!=null)")"
+while [ -z "${IMAGE_UUID}" ]; do
+  OUTPUT=""
+
+  if [ -n "${IMAGE_UUID}" ]; then
+    OUTPUT="${IMAGE} already exist, skip image upload"
+  else
+    IMAGE_UUID=$(
+    >&2 echo "Starting image check for ${IMAGE}"
+    # If another upload is running wait 5 min
+    IMAGE_CREATED_NEWEST_ELAPSED=0
+    while [ ${IMAGE_CREATED_NEWEST_ELAPSED} -lt ${TIMEOUT} ]; do
+      [ $IMAGE_CREATED_NEWEST_ELAPSED -ne 0 ] && sleep 10
+      IMAGES_SAVING_UUID="$(openstack image list -f json | jq -r "map(select(.Name==\"${IMAGE}\")) | map(select(.Status==\"saving\")) | .[].ID | select (.!=null)")"
+      IMAGE_CREATED_NEWEST=0
+      for IMAGE_SAVING_UUID in $IMAGES_SAVING_UUID; do
+        IMAGE_CREATED=$(openstack image show -f json ${IMAGE_SAVING_UUID} | jq -r .created_at | xargs -i date "+%s" -d "{}")
+        if [ ${IMAGE_CREATED} -gt ${IMAGE_CREATED_NEWEST} ]; then
+          IMAGE_CREATED_NEWEST=${IMAGE_CREATED}
+        fi
+      done
+      IMAGE_CREATED_NEWEST_ELAPSED=$(echo $(date "+%s") - ${IMAGE_CREATED_NEWEST} | bc)
+      IMAGE_UUID="$(openstack image list -f json | jq -r "map(select(.Name==\"${IMAGE}\")) | map(select(.Status==\"active\")) | .[0] | .ID | select (.!=null)")"
+      if [ ${IMAGE_UUID} ]; then
+        echo ${IMAGE_UUID}
+        exit 0
       fi
     done
-    IMAGE_CREATED_NEWEST_ELAPSED=$(echo $(date "+%s") - ${IMAGE_CREATED_NEWEST} | bc)
-    IMAGE_UUID="$(openstack image list -f json | jq -r "map(select(.Name==\"${IMAGE}\")) | map(select(.Status==\"active\")) | .[0] | .ID | select (.!=null)")"
-    if [ ${IMAGE_UUID} ]; then
-      echo ${IMAGE_UUID}
-      exit 0
-    fi
-  done
 
-  until curl -s https://swift.entercloudsuite.com/v1/KEY_1a68c22a99cd4e558054ede2c878929d/automium-catalog-images/openstack/${IMAGE}.qcow2 | openstack image create ${IMAGE} -f json >/dev/null; do
-    sleep 10
-    openstack image list -f json | jq -r "map(select(.Name==\"${IMAGE}\")) | map(select(.Status!=\"saving\")) | map(select(.Status!=\"active\")) | .[0] | .ID | select(.!=null)" | while read uuid; do openstack image delete $uuid > /dev/null; done
-  done
+    >&2 echo "Starting image upload"
+    COUNT=0
+    until timeout ${TIMEOUT} curl -s https://swift.entercloudsuite.com/v1/KEY_1a68c22a99cd4e558054ede2c878929d/automium-catalog-images/openstack/${IMAGE}.qcow2 | openstack image create ${IMAGE} -f json >/dev/null; do
+      let COUNT++
+      sleep 10
+      openstack image list -f json | jq -r "map(select(.Name==\"${IMAGE}\")) | map(select(.Status!=\"saving\")) | map(select(.Status!=\"active\")) | .[0] | .ID | select(.!=null)" | while read uuid; do openstack image delete $uuid > /dev/null; done
+      if COUNT > 5; then
+        >&2 echo 'Number of retry exceeded ($COUNT) during image upload ${IMAGE} - retrying from start'
+        continue
+      fi
+    done
 
-  IMAGE_UUID=$(openstack image list -f json | jq -r "map(select(.Name==\"${IMAGE}\")) | map(select(.Status==\"active\")) | .[0] | .ID | select(.!=null)")
-  openstack image set --property hw_vif_multiqueue_enabled='true' ${IMAGE_UUID} > /dev/null
-  echo ${IMAGE_UUID}
-  )
-fi
+    IMAGE_UUID=$(openstack image list -f json | jq -r "map(select(.Name==\"${IMAGE}\")) | map(select(.Status==\"active\")) | .[0] | .ID | select(.!=null)")
+    openstack image set --property hw_vif_multiqueue_enabled='true' ${IMAGE_UUID} > /dev/null
+    echo ${IMAGE_UUID}
+    )
+  fi
+done
 
 OUTPUT=$(echo $OUTPUT | cut -c 1-20)
 jq -r -n --arg image_uuid "${IMAGE_UUID}" --arg output "${OUTPUT}" '{"image_uuid":$image_uuid,"output":$output}'
